b52f90edb7b8db181b18a6770a91c90dfa22e49a
ERECTUMTOUCHER <75875960+ERECTUMTOUCHER@users.noreply.github.com>
2024-03-07 21:10:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+// JavaScript functions for image inspection
+document.addEventListener('DOMContentLoaded', function() {
+    var galleryImages = document.querySelectorAll('.cs-gallery img');
+    var overlay = document.querySelector('.overlay');
+    var overlayImage = document.getElementById('overlayImage');
 
+    galleryImages.forEach(function(image) {
+        image.addEventListener('click', function() {
+            var fullImagePath = this.getAttribute('data-full');
+            overlayImage.src = fullImagePath;
+            overlay.style.display = 'block';
+        });
+    });
+
+    overlay.addEventListener('click', function() {
+        this.style.display = 'none';
+    });
+});
+
+$(document).ready(function() {
+
+    // slick variables
+    var sliderVideoList = $('.series-videos-carousel'),
+        sliderLightboxList = $('.person-videos-carousel'),
+        visibleVideoSlidesDesktop = 3,
+        visibleLightboxSlides = 3,
+        visibleSlidesMobile = 1;
+    
     // video slider
     sliderVideoList.slick({
         infinite: false,
